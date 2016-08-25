355b44f8be70be2d557a8d3d820047ff14bfe9fe
zjfeng <zjfeng18@foxmail.com>
2016-08-25 22:40:22 +0800

    增加内容清洗与远程图片下载

--- pa.py
+++ pa.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import requests,time,os # requests作为我们的html客户端
+import requests,time,os,re # requests作为我们的html客户端
 from pyquery import PyQuery as Pq # pyquery来操作dom
 # https://segmentfault.com/a/1190000002549756
 # 文章详情页 http://www.0731gch.com/paixie/bianmi/18086.html
@@ -28,14 +28,50 @@ class Getshow(object):
    # 暂时保存成文档
     def save(self):
         sDir='d:/test/'
+        #图片地址
+        img_dir = 'img'
         if os.path.exists(sDir)==False:
             os.mkdir(sDir)
         sName = sDir+str(int(time.time()))+'.txt'
         print('正在下载'+self.title+'文章')
-        m = self.title+'\n'+self.content
+        m = self.clearInput(self.title)+'\n'+self.clearInput(self.content)
+        #批量替换旧内容中的图片的路径
+        img_patt = re.compile('src=".*?/(\w+\.\w+)"')
+        new_m = img_patt.sub(r'src="./%s/\1"'%img_dir,m)
         with open(sName,'wb') as file:
-            file.write(m.encode())
+            file.write(new_m.encode())
         file.close()
+        #真正下载图片
+        img_patt = re.compile('src="(.*?)"')
+        img_patt = img_patt.findall(m)
+        i =0
+        for img in img_patt:
+            i+=1
+            #图片名称
+            img_name = os.path.join(img_dir,img.split('/')[-1])
+             #获取图片资源
+            if os.path.exists(sDir+img_dir)==False:
+                os.mkdir(sDir+img_dir)
+            #合并路径
+            imgpath=os.path.join(sDir,img_name)
+            f = open(imgpath, 'wb')
+            f.write(requests.get(img, stream=True).content)
+            f.close()
+
+    def clearInput(self,txt):
+        txt=txt.replace('长沙医博肛肠医院','瑞安九龙医院')
+        txt=txt.replace('长沙','瑞安')
+        txt=txt.replace('医博','九龙')
+        txt=txt.replace('湖南','湖南')
+        txt=txt.replace('YEBO','九龙')
+        txt=txt.replace('0731-89917766','0577-65913333')
+        txt=txt.replace('0731-82296120','0577-65913333')
+        txt=txt.replace('800040095','2812882905')
+        txt=txt.replace('YiBo','九龙')
+        # 正则替换
+        # text=re.sub('\[[0-9]*\]','',text)
+        return txt
+
 
 #栏目页 http://www.0731gch.com/paixie/bianmi/index_26_2.html
 #getpages限制取几页
